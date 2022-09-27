5f698270b652ff69a560df4f815f5a4b2067514f
bgbester <90190484+bgbester@users.noreply.github.com>
2022-09-27 21:25:58 +0200

    Created Repo

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repositories/CategoriesRepository.cs
@@ -0,0 +1,25 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repositories
+{
+    public class CategoriesRepository
+    {
+        private readonly ConnectedOfficeContext _context = new ConnectedOfficeContext();
+
+        //GET: Categories
+        public List<Category> Getall()
+        {
+            return _context.Category.ToList();
+        }
+
+        /*// GET: Categories/Details/5
+        public List<Category> Getall()
+        {
+            return _context.Category.ToList();
+        }
+        */
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repositories/DevicesRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repositories
+{
+    public class DevicesRepository
+    {
+        private readonly ConnectedOfficeContext _context = new ConnectedOfficeContext();
+    }
+}
